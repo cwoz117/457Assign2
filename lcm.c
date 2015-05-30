337db598515c94ec5df7a38565cd9e2503fc742b
root <root@cpsc457.cloud.cybera.ca>
2015-05-30 22:31:39 +0000

    starting commit

---
+++ lcm.c
@@ -0,0 +1,33 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*	            				File:	lcm.c		     *
+*	             				Course:	CPSC457		     *
+*	           				Author:	Chris Wozniak        *
+*                  				SID:	10109820             *
+*	         							     *
+*	    This program is a multi-threaded system that determines the      *
+*	lowest common multiple when provided with a file of integers.	     *
+*       It also allows the user to expand how many threads the               *
+*       application is capable of running. If left blank the function        *
+*	will default to 3.                                                   *
+*									     *
+*	EXAMPLE: ./lcm <file-of-int> <number-of-threads>		     *
+*									     *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*END OF FILE END OF FILE END OF FILE END OF FILE END OF FILE END OF FILE END *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
